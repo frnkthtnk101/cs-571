5a7abd4524b8417af41283be34fcfc840f954b2d
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2018-02-20 19:27:24 -0500

    saferm done

--- Assignment4/saferm.sh
+++ Assignment4/saferm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 WORKINGDIR=$HOME/.JUNK
 CURRENTPATH=`pwd`
+#sace first
 filename=`date "+$1:%y-%m-%d-%H.%M.%S"`
 if [ ! -d $WORKINGDIR ]
 	then
@@ -14,4 +15,16 @@ tar  --force-local -cf  $filename.tgz *
 mv $filename.tgz $WORKINGDIR/$filename.tgz
 cd ..
 rm -rf $filename
-
+#kill
+echo what is happening to files
+for i in `ls *.tgz`;
+do
+	FiletempDate=`echo $i | cut -f2 -d":" | awk  -F "\-|\." '{print $1$2$3}'`
+	FileDate=`date -d "$FiletempDate 2 days" +%y%m%d`
+	today=`date '+%y%m%d'`
+	if [ $FileDate -lt $today ];
+	then
+		echo $i was deleted... It has been two days
+		rm $i
+	fi
+done
